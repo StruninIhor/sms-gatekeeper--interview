a87b8b8ffa5572ca89b2be0b7162f1400afd0935
Ihor Strunin <strunin.ihor@gmail.com>
2025-03-03 15:01:38 -0800

    feat: added project gitignore, ISmsInterface and controller

---
+++ src/backend/SmsGatekeeper/SmsGatekeeper/Controllers/SmsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmsGatekeeper.Models;
+using SmsGatekeeper.Services;
+
+namespace SmsGatekeeper.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SmsController(ISmsService smsService) : ControllerBase
+    {
+        private readonly ISmsService _smsService = smsService ?? throw new ArgumentNullException(nameof(smsService));
+
+        [HttpGet]
+
+        /// <summary>
+        /// Returns whether this phoneNumber can send an SMS message.
+        /// </summary>
+        /// <param name="phoneNumber">Phone number in format "1NPANXXXXX", where 1 - country code, NPA - area code, NXX - exchange code, XXXX - subscriber number. Now only supports US/Canada</param>
+        /// <returns></returns>
+        public async Task<IActionResult> CanSendSms(PhoneNumber phoneNumber) => 
+            Ok(new
+                {
+                    CanSend = await _smsService.CanSendSms(phoneNumber)
+                });
+    }
+}
---
+++ src/backend/SmsGatekeeper/SmsGatekeeper/Models/SendMessageRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace SmsGatekeeper.Models
+{
+    public class SendMessageRequest
+    {
+        [Required]
+        [JsonConverter(typeof(PhoneNumber.PhoneNumberConverter))]
+        public PhoneNumber PhoneNumber { get; set; }
+    }
+}
