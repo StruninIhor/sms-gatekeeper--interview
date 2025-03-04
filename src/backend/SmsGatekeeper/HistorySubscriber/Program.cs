83d0d7ab3e5f5ca01304f3fa85908309efbd4f54
Ihor Strunin <strunin.ihor@gmail.com>
2025-03-04 14:54:43 -0800

    feat: historical data saving added
    Added separate worker service that stores data using redis pub/sub

---
+++ src/backend/SmsGatekeeper/HistorySubscriber/Program.cs
@@ -0,0 +1,44 @@
+﻿using Serilog;
+using StackExchange.Redis;
+
+namespace HistorySubscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // var context 
+            var mux = ConnectionMultiplexer.Connect("localhost");
+            var subscriber = mux.GetSubscriber();
+
+            subscriber.Subscribe(RedisChannel.Pattern("rate_limit_events"), (channel, type) =>
+            {
+                var key = GetKey(channel);
+            });
+
+            Log.Information("History Subscriber is running!");
+
+            Console.ReadLine();
+        }
+
+        static void ConfigureLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+              .MinimumLevel.Information()
+              .WriteTo.Console()
+              .CreateLogger();
+        }
+
+        static string GetKey(string channel)
+        {
+            var index = channel.IndexOf(':');
+
+            if (index >= 0 && index < channel.Length - 1)
+            {
+                return channel[(index + 1)..];
+            }
+
+            return channel;
+        }
+    }
+}
---
+++ src/backend/SmsGatekeeper/SmsGatekeeper.Domain/SmsContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmsGatekeeper.Domain.Models;
+
+namespace SmsGatekeeper.Domain
+{
+    public class SmsContext : DbContext
+    {
+        public DbSet<HistoryRecord> HistoryRecords { get; set; }
+
+        public SmsContext(DbContextOptions<SmsContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HistoryRecord>().HasIndex(x => x.AccountId);
+            modelBuilder.Entity<HistoryRecord>().HasIndex(x => x.PhoneNumber);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
